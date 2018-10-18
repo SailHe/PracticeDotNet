ff0d1fff48d08e878b87938a9552dbcedcd27aa4
SailHe <396103442@qq.com>
2018-10-18 15:08:39 +0800

    添加项目文件。

---
+++ PracticeDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
