e5dc0170002179a858dea0569d6d732a1c64c32a
SailHe <396103442@qq.com>
2018-11-21 14:44:24 +0800

    在win forms项目中添加ADO.NET数据实体模型

--- WinFormsApp/src/Form11_21.cs
+++ WinFormsApp/src/Form11_21.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using LearnDotNet;
+// using LearnDotNet;
 
 namespace WinFormsApp.src
 {
@@ -25,12 +25,12 @@ namespace WinFormsApp.src
 
         static void solve1_11_21()
         {
-            List<StudentInfo> studentS = new List<StudentInfo>();
-            intMapString gidMapGname = null;
-            var gredeIdMapName = LearnDotNet.Program.calcAllClass(out gidMapGname);
-            studentS = LearnDotNet.Program.calcAll(gidMapGname);
-            // ShellFor_11_15(studentS, gredeIdMapName);
-            // LearnDotNet.Program.saveAll(studentS);
+            // List<StudentInfo> studentS = new List<StudentInfo>();
+            // intMapString gidMapGname = null;
+            // var gredeIdMapName = LearnDotNet.Program.calcAllClass(out gidMapGname);
+            // studentS = LearnDotNet.Program.calcAll(gidMapGname);
+            // // ShellFor_11_15(studentS, gredeIdMapName);
+            // // LearnDotNet.Program.saveAll(studentS);
         }
 
         private void button_add_Click(object sender, EventArgs e)
