6c9a54484a2fc6b4ffa1f935d672940a7b28b642
SailHe <396103442@qq.com>
2018-12-06 17:05:17 +0800

    自定义类型的LINKQ 原生Sql

--- WinFormsApp/src/Form12_6.cs
+++ WinFormsApp/src/Form12_6.cs
@@ -11,11 +11,19 @@ using System.Data.Linq;
 using System.Data.Linq.Mapping;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using System.Data.Entity.Infrastructure;
 
 namespace WinFormsApp.src
 {
     public partial class Form12_6 : Form
     {
+        // partial 局部类型的概念。局部类型允许我们将一个类、结构或接口分成几个部分，分别实现在几个不同的.cs文件中。
+        class StuDTO
+        {
+            public string sname { get; set; }
+            public int sid { get; set; }
+        }
+
         public Form12_6()
         {
             InitializeComponent();
@@ -27,12 +35,16 @@ namespace WinFormsApp.src
             string sql = "SELECT stu.sid AS 学号, stu.sname AS 姓名, stu.ssexy AS 性别" +
                 ", stu.sbdate AS 出生日期, ug.gname AS 班级, stu.stele AS 电话" +
                 ", sc.score1 AS 成绩 FROM ustudent stu INNER JOIN ugrade ug USING(gid) JOIN usc sc USING(sid)";
-            var results = PlaygroundForm.dbContext.Database.SqlQuery<string>(
-                    "SELECT sname FROM ustudent WHERE sid = @sid", new MySqlParameter("@sid", 32006005)
+            // 自定义类型需要geter和seter
+            DbRawSqlQuery<StuDTO> results = PlaygroundForm.dbContext.Database.SqlQuery<StuDTO>(
+                    "SELECT sname, sid FROM ustudent WHERE sid = @sid", new MySqlParameter("@sid", 32006005)
                 );
+            var temp = results.ToList<StuDTO>();
+            // 若包涵多个元素 会抛异常 详参API
+            // var temp1 = results.SingleOrDefault();
+
             foreach (var r in results)
             {
-                // Console.WriteLine("{0}\t{1}\t{2}\t\t\t{3}", r.sid, r.sname, r.cname.Trim(), r.score);
                 Console.WriteLine(r.ToString());
             }
             new LINQEF.LINQExam().queryScoreWithFunc("12005001");
@@ -53,10 +65,10 @@ namespace WinFormsApp.src
         }
         /**
          * EF提供一组方法用来执行原生的SQL.
-         * 有以下三种:
-         * 1.DbSet.SqlQuery
-         * 2.Database.SqlQuery
-         * 3.Database.ExecuteSqlCommand
+         * 原生SQL执行查询有以下三种:
+         * 1.DbSet.SqlQuery -> 需要返回实体模型（context会跟踪，等效于LINQ方式）
+         * 2.Database.SqlQuery -> 需要返回其他类型
+         * 3.Database.ExecuteSqlCommand -> 原生SQL执行更新
          * 来源@see http://www.voidcn.com/article/p-qjdrcntb-xv.html
          * 详情参考VS本地API说明
          * 以下分别演示三种用法
@@ -73,10 +85,24 @@ namespace WinFormsApp.src
         }
         void LINKQ_NaiveSQL_Sample2()
         {
-            var results = PlaygroundForm.dbContext.Database.SqlQuery<string>(
+            // 如果返回复杂的结构需要自行定义
+            DbRawSqlQuery<string> results = PlaygroundForm.dbContext.Database.SqlQuery<string>(
                     "SELECT sname FROM ustudent WHERE sid = @sid", new MySqlParameter("@sid", 32006005)
                 );
             foreach (var r in results)
+            {
+                Console.WriteLine(r.ToString());
+            }
+
+            // 自定义类型需要geter和seter
+            DbRawSqlQuery<StuDTO> resultDataSet = PlaygroundForm.dbContext.Database.SqlQuery<StuDTO>(
+                    "SELECT sname, sid FROM ustudent WHERE sid = @sid", new MySqlParameter("@sid", 32006005)
+                );
+            var temp = resultDataSet.ToList<StuDTO>();
+            // 若包涵多个元素 会抛异常 详参API
+            // var temp1 = resultDataSet.SingleOrDefault();
+
+            foreach (var r in resultDataSet)
             {
                 Console.WriteLine(r.ToString());
             }
