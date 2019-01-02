8cb326bc1de04a90b485fd5ba803c1f4e42200fb
SailHe <396103442@qq.com>
2019-01-02 19:07:40 +0800

    新建的 WebForms项目

---
+++ WebApp/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace WebApp
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
+
+            // @see https://docs.microsoft.com/en-us/previous-versions/cc668201(v=vs.140)
+            routes.MapPageRoute("",
+            "Category/{action}/{categoryName}",
+            "~/categoriespage.aspx");
+        }
+    }
+}
